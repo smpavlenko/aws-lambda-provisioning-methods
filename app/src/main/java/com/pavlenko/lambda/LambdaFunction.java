7d8250ba3c09ca29eb74fe236f04c179df902981
Sergii Pavlenko <sm.pavlenko@gmail.com>
2018-10-18 22:08:54 +0200

    Created initial function

---
+++ app/src/main/java/com/pavlenko/lambda/LambdaFunction.java
@@ -0,0 +1,21 @@
+package com.pavlenko.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LambdaFunction implements RequestHandler<S3Event, Long>
+{
+    private static final Logger logger = LogManager.getLogger();
+
+
+    @Override
+    public Long handleRequest(S3Event s3Event, Context context)
+    {
+        logger.info("Function is invoked");
+        // TODO to implement
+        return null;
+    }
+}
