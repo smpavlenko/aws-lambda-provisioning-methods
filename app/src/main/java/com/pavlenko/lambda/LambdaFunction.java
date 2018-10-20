32a854e1af9b16f23b4b141eb44cac2cf2c83f31
Sergii Pavlenko <sm.pavlenko@gmail.com>
2018-10-20 11:04:56 +0200

    Formatted LambdaFunction

--- app/src/main/java/com/pavlenko/lambda/LambdaFunction.java
+++ app/src/main/java/com/pavlenko/lambda/LambdaFunction.java
@@ -6,14 +6,12 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LambdaFunction implements RequestHandler<S3Event, Long>
-{
+public class LambdaFunction implements RequestHandler<S3Event, Long> {
     private static final Logger logger = LogManager.getLogger();
     private final FunctionProcessor processor = new FunctionProcessor();
 
     @Override
-    public Long handleRequest(S3Event s3Event, Context context)
-    {
+    public Long handleRequest(S3Event s3Event, Context context) {
         logger.info("Function is invoked");
         return processor.process(s3Event);
     }
