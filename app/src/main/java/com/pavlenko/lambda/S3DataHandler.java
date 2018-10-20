b89dc7fbbf33e250f54989cc7baacdfc6a7ce289
Sergii Pavlenko <sm.pavlenko@gmail.com>
2018-10-20 11:04:20 +0200

    Formatted S3DataHandler

--- app/src/main/java/com/pavlenko/lambda/S3DataHandler.java
+++ app/src/main/java/com/pavlenko/lambda/S3DataHandler.java
@@ -11,47 +11,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public class S3DataHandler
-{
+public class S3DataHandler {
     private static final Logger logger = LogManager.getLogger();
     private final AmazonS3 amazonS3;
 
 
-    public S3DataHandler(final AmazonS3 amazonS3)
-    {
+    public S3DataHandler(final AmazonS3 amazonS3) {
         this.amazonS3 = amazonS3;
     }
 
-
-    public String retrieve(
-        final String s3Key,
-        final String s3Bucket)
-    {
+    public String retrieve(final String s3Key,
+        final String s3Bucket) {
         logger.info("Retrieving objects from {}/{}", s3Bucket, s3Key);
         final S3Object object = amazonS3.getObject(new GetObjectRequest(s3Bucket, s3Key));
-        if (object == null)
-        {
+        if (object == null) {
             return null;
         }
-        try (final InputStream objectData = object.getObjectContent())
-        {
+        try (final InputStream objectData = object.getObjectContent()) {
             return IOUtils.toString(objectData);
-        }
-        catch (final IOException ex)
-        {
+        } catch (final IOException ex) {
             logger.error("Failed to read log file" + ex.getMessage(), ex);
         }
         return null;
     }
 
-
-    public void put(
-        final String s3Key,
-        final String s3Bucket, List<String> sessionIds)
-    {
+    public void put(final String s3Key,
+        final String s3Bucket, List<String> sessionIds) {
         logger.info("Putting objects to {}/{}", s3Bucket, s3Key);
         amazonS3.putObject(s3Bucket, s3Key, String.join(System.getProperty("line.separator"), sessionIds));
     }
-
 
 }
